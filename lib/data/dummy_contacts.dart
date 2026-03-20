1c70d3641603563b6c7969267342194f93e2d19f
marcelinobudi <marcelinobprakasya@gmail.com>
2026-03-20 11:22:40 +0700

    Add dummy data for contact

---
+++ lib/data/dummy_contacts.dart
@@ -0,0 +1,157 @@
+import 'package:contacts/models/contact.dart';
+
+List<Contact> contactList = [
+  // A
+  Contact(
+    firstName: 'Aditya',
+    lastName: 'Pratama Putra',
+    number: '6281234567890',
+  ),
+  Contact(
+    firstName: 'Andini',
+    lastName: 'Putri Kusuma',
+    number: '6281244556677',
+  ),
+  Contact(
+    firstName: 'Aryan',
+    lastName: 'Saputra Jaya',
+    number: '6281211223344',
+  ),
+
+  // B
+  Contact(
+    firstName: 'Budi',
+    lastName: 'Santoso Utama',
+    number: '6281323456789',
+  ),
+  Contact(firstName: 'Bayu', lastName: 'Setiawan Adi', number: '6281355667788'),
+  Contact(
+    firstName: 'Bella',
+    lastName: 'Clarissa Putri',
+    number: '6281399001122',
+  ),
+
+  // C
+  Contact(
+    firstName: 'Candra',
+    lastName: 'Wijaya Kusuma',
+    number: '6281133445566',
+  ),
+  Contact(
+    firstName: 'Christian',
+    lastName: 'Hadi Permana',
+    number: '6281177889900',
+  ),
+  Contact(
+    firstName: 'Citra',
+    lastName: 'Lestari Indah',
+    number: '6281198765432',
+  ),
+
+  // D
+  Contact(
+    firstName: 'Dedi',
+    lastName: 'Kurniawan Syah',
+    number: '6285643218765',
+  ),
+  Contact(
+    firstName: 'Dina',
+    lastName: 'Mahendra Putri',
+    number: '6285611223344',
+  ),
+  Contact(
+    firstName: 'Donny',
+    lastName: 'Pangestu Alam',
+    number: '6285655667788',
+  ),
+
+  // E
+  Contact(
+    firstName: 'Eka',
+    lastName: 'Wijaya Pratama',
+    number: '6287811223344',
+  ),
+  Contact(
+    firstName: 'Endah',
+    lastName: 'Permata Sari',
+    number: '6287855667788',
+  ),
+  Contact(
+    firstName: 'Erick',
+    lastName: 'Sanjaya Putra',
+    number: '6287899001122',
+  ),
+
+  // F
+  Contact(
+    firstName: 'Fajri',
+    lastName: 'Ramadhan Putra',
+    number: '6281911223344',
+  ),
+  Contact(
+    firstName: 'Fani',
+    lastName: 'Rahmawati Suci',
+    number: '6281955667788',
+  ),
+  Contact(
+    firstName: 'Felicia',
+    lastName: 'Salim Wijaya',
+    number: '6281999001122',
+  ),
+
+  // G
+  Contact(
+    firstName: 'Gilang',
+    lastName: 'Permana Aji',
+    number: '6282199001122',
+  ),
+  Contact(
+    firstName: 'Gita',
+    lastName: 'Gutawa Maharani',
+    number: '6282133445566',
+  ),
+  Contact(
+    firstName: 'Gunawan',
+    lastName: 'Malik Ibrahim',
+    number: '6282177889900',
+  ),
+
+  // H
+  Contact(
+    firstName: 'Hana',
+    lastName: 'Anindita Sari',
+    number: '6285211223344',
+  ),
+  Contact(
+    firstName: 'Hendra',
+    lastName: 'Suwarno Putra',
+    number: '6285277889900',
+  ),
+  Contact(firstName: 'Hesti', lastName: 'Putri Utami', number: '6285233445566'),
+
+  // I
+  Contact(
+    firstName: 'Indra',
+    lastName: 'Kusuma Bakti',
+    number: '6281277889900',
+  ),
+  Contact(
+    firstName: 'Irfan',
+    lastName: 'Bachdim Surya',
+    number: '6281211223344',
+  ),
+  Contact(firstName: 'Irma', lastName: 'Suryani Dewi', number: '6281233445566'),
+
+  // J
+  Contact(firstName: 'Jerry', lastName: 'Yanuar Gani', number: '6281377889900'),
+  Contact(
+    firstName: 'Jessica',
+    lastName: 'Mila Agnesia',
+    number: '6281333445566',
+  ),
+  Contact(
+    firstName: 'Joko',
+    lastName: 'Susilo Bambang',
+    number: '6281311112222',
+  ),
+];
