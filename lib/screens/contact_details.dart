6f402491c0a5df981122acb6da8a427992bb8bd9
marcelinobudi <marcelinobprakasya@gmail.com>
2026-03-21 08:54:06 +0700

    set up add_contact, contact_details, edit_contact

---
+++ lib/screens/contact_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContactDetailsScreen extends StatelessWidget {
+  const ContactDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
